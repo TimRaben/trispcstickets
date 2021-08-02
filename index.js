83cf4143e307c385985943e17cc5cedf03e975ac
TimRaben <87567520+TimRaben@users.noreply.github.com>
2021-08-02 14:09:43 +0200

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ client.on('message', async message => {
         message.channel.send("**Succesvol Reactie Bericht aangemaakt!**")
     }
 
-    if(command == "close", "done",) {
+    if(command == "close") {
         if(!args[0]) return message.channel.send("**FOUT!** - Geef een geldige reden op!")
         if(!message.channel.name.includes("sollicitatie-", "vraag-", "overig-", "event-", "management-",)) return message.channel.send("**FOUT!** - Deze Ticket staat niet in de Database! Dus het is niet mogelijk om dit kanaal te sluiten!")
         message.channel.delete();
